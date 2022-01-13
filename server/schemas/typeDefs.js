8f4e5887714add8bcfdc31e98ff7b79ae960cb40
Jeffery <jefferywojo1998@gmail.com>
2022-01-12 21:05:00 -0600

    getting help

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+// import the gql
+const { gql } = require('apollo-server-express');
+
+// create typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(content: BookInfo!): User
+        removeBook(bookId: String!): User
+    }
+    input BookInfo {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
+
+// export typeDefs
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const { ApolloServer } = require("apollo-server-express");
 const routes = require('./routes');
-const {authMiddleware} = require('./utils/auth');
-
+const { ApolloServer } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context: authMiddleware
 });
-
+server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -20,14 +18,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!!!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
