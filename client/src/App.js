6a3d056f002c53715aa923f2436e659351807c9b
Jeffery <jefferywojo1998@gmail.com>
2022-01-14 01:01:15 -0600

    app is fix. need to fix login/signup

--- client/src/App.js
+++ client/src/App.js
@@ -3,41 +3,31 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-
-// import apollo server deets
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
-// import setContext - included in authLink
 import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
-  // url/endpoint for all GraphQL requests
+
   uri: '/graphql',
 });
-
-// setContext obtains token and attaches to http headers
 const authLink = setContext((_, { headers }) => {
-  // use getItem method from local storage
   const token = localStorage.getItem('id_token');
   return {
     headers: {
       ...headers,
-      // with token now obtained
       authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
 
-// bring together the httpLink + authLink objects for API requests
 const client = new ApolloClient({
-  // combo
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
-    // added in apollo
     <ApolloProvider client={client}>
       <Router>
         <>
