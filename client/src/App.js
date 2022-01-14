2c5d8e82fd294cc1a5ece534a1fb5a67fb9b9748
Jeffery <jefferywojo1998@gmail.com>
2022-01-13 23:10:48 -0600

    adding graphqql and apllloclient to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,52 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// import apollo server deets
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// import setContext - included in authLink
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  // url/endpoint for all GraphQL requests
+  uri: '/graphql',
+});
+
+// setContext obtains token and attaches to http headers
+const authLink = setContext((_, { headers }) => {
+  // use getItem method from local storage
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      // with token now obtained
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// bring together the httpLink + authLink objects for API requests
+const client = new ApolloClient({
+  // combo
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    // added in apollo
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
